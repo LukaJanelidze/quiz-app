449f1f9fd6c9d9efd30679b1fccf692a191961be
Luka Janelidze <lukajanelidze5@gmail.com>
2024-07-27 06:20:01 +0400

    feats: add current question feature, add some css

--- src/components/quiz/Quiz.tsx
+++ src/components/quiz/Quiz.tsx
@@ -1,41 +1,45 @@
-import { useState } from "react"
-import quizData from "./../../data.json"
-import Question from "./../question/Question"
-import "./Quiz.css"
+import { useState } from "react";
+import quizData from "./../../data.json";
+import Question from "./../question/Question";
+import "./Quiz.css";
 
 export default function Quiz() {
-    const [currentQuestionIndex, setCurrentQuestionIndex] = useState<number>(0)
-    const [score, setScore] = useState<number>(0)
-    const [showScore, setShowScore] = useState<boolean>(false)
+    const [currentQuestionIndex, setCurrentQuestionIndex] = useState<number>(0);
+    const [score, setScore] = useState<number>(0);
+    const [showScore, setShowScore] = useState<boolean>(false);
 
-    console.log(score)
-    
     function onSelectOption(selectedOption: string) {
-        if(selectedOption === quizData[currentQuestionIndex].correctAnswer) {
-            setScore(score + 1)
+        if (selectedOption === quizData[currentQuestionIndex].correctAnswer) {
+            setScore(prevScore => prevScore + 1);
         }
 
-        const nextQuestionIndex = currentQuestionIndex + 1
-        if(nextQuestionIndex < quizData.length) {
-            setCurrentQuestionIndex(currentQuestionIndex + 1)
+        const nextQuestionIndex = currentQuestionIndex + 1;
+        if (nextQuestionIndex < quizData.length) {
+            setCurrentQuestionIndex(nextQuestionIndex);
         } else {
-            setShowScore(true)
+            setShowScore(true);
         }
     }
 
-    return(
+    const { question, options } = quizData[currentQuestionIndex];
+
+    return (
         <div>
+            {!showScore && (
+                <h1 className="current-question">Question {currentQuestionIndex + 1}</h1>
+            )}
+
             {showScore ? (
                 <div className="quiz-score">
-                    <h1>Your Score: {score} / {quizData.length}</h1>
+                    <h3>Your Score: {score} / {quizData.length}</h3>
                 </div>
-            ) : (<Question 
-                    question={quizData[currentQuestionIndex].question}
-                    options={quizData[currentQuestionIndex].options}
+            ) : (
+                <Question
+                    question={question}
+                    options={options}
                     onSelectOption={onSelectOption}
                 />
             )}
-            
         </div>
-    )
+    );
 }
